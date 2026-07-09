a0dcc4ee3c42fbef209d147a760e02896a0d46bb
ALUMNO <ALUMNO@ALUMNO>
2026-07-09 10:19:21 -0500

    primer cargue

---
+++ src/main/java/com/mycompany/simuladorbancario/SimuladorBancario.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.simuladorbancario;
+
+/**
+ *
+ * @author ALUMNO
+ */
+public class SimuladorBancario {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
